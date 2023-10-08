fb63214b181c7239f784c4914ab79a057903a9c1
edvaldoLeite81 <eguilhermeleite@uni9.edu.br>
2023-10-08 08:26:22 -0300

    update

--- valoresLiterais/src/Main.java
+++ valoresLiterais/src/Main.java
@@ -1,18 +1,23 @@
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         //default double por isso necessario explicita "f"
-        float salario = 4_580.00f;
+        float salario = 4_630.00f;
         float rendimento = 0.5f;
+        Locale locale = new Locale("pt", "BR");
+        NumberFormat moeda = NumberFormat.getCurrencyInstance(locale);
 
-        System.out.println("**************************");
-        System.out.println("Salário Mensal: " + salario);
-        System.out.println("Rendimento Mensal: " + rendimento +"%");
-        System.out.println("**************************");
+        System.out.println("****************************");
+        System.out.println("Salário Mensal: " + moeda.format(salario));
+        System.out.println("Rendimento Mensal: " + rendimento + "%");
+        System.out.println("****************************");
 
-        for (int mes = 1; mes <= 12; mes++){
+        for (int mes = 1; mes <= 12; mes++) {
             salario += salario * rendimento / 100;
-            System.out.printf("Salário ao final do %d° mês: R$ %,.2f%n",mes,salario);
+            System.out.printf("%nSalário ao final do %d° mês: R$ %,.2f%n", mes, salario);
+            System.out.println("********* Com NumberFormat: " + moeda.format(salario));
         }
     }
 }
